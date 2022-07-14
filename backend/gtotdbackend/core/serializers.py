c0523f4e988a22fd3b52ec5e82e43fd7d656d890
Brandon <brabick@gmail.com>
2022-07-14 14:46:03 -0600

    Initial commit. Create core backend application for user authentication. Create mock email server for resetting passwords.

---
+++ backend/gtotdbackend/core/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework.serializers import ModelSerializer
+from .models import User
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'email', 'password']
+        extra_kwargs = {
+            'password':
+                {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        # Extract password
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        # Set the password
+        if password is not None:
+            instance.set_password(password)
+        # Save
+        instance.save()
+        # Return without the password
+        return instance
