9022acbbf69e78062a4ba0ee57e08d547061d5b8
Brandon <brabick@gmail.com>
2022-10-28 18:00:39 -0600

    Add content to profile page. Correct profile requests

--- backend/gtotdbackend/gtotdbackend/urls.py
+++ backend/gtotdbackend/gtotdbackend/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from AngularGTOTDApp.backend.gtotdbackend.gtotdbackend import settings
+from django.conf.urls.static import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
